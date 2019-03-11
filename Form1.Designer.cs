484caf6935a71944f350c24666d97fcf3fecc481
Walkinged <43610419+Walkinged@users.noreply.github.com>
2019-03-11 19:22:21 +0000

    Add files via upload
    Adding items to a list box, removing items and changing fonts

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace list_box_add_tems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clearSelectBtn = new System.Windows.Forms.Button();
+            this.fontBtn = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.SuspendLayout();
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(30, 57);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(151, 23);
+            this.addBtn.TabIndex = 0;
+            this.addBtn.Text = "AddItems to List";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(106, 270);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Clear List";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(187, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(204, 264);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // clearSelectBtn
+            // 
+            this.clearSelectBtn.Location = new System.Drawing.Point(54, 328);
+            this.clearSelectBtn.Name = "clearSelectBtn";
+            this.clearSelectBtn.Size = new System.Drawing.Size(127, 23);
+            this.clearSelectBtn.TabIndex = 4;
+            this.clearSelectBtn.Text = "Clear selected items";
+            this.clearSelectBtn.UseVisualStyleBackColor = true;
+            this.clearSelectBtn.Click += new System.EventHandler(this.clearSelectBtn_Click);
+            // 
+            // fontBtn
+            // 
+            this.fontBtn.Location = new System.Drawing.Point(81, 299);
+            this.fontBtn.Name = "fontBtn";
+            this.fontBtn.Size = new System.Drawing.Size(100, 23);
+            this.fontBtn.TabIndex = 5;
+            this.fontBtn.Text = "Change Font";
+            this.fontBtn.UseVisualStyleBackColor = true;
+            this.fontBtn.Click += new System.EventHandler(this.fontBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 367);
+            this.Controls.Add(this.fontBtn);
+            this.Controls.Add(this.clearSelectBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.addBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button clearSelectBtn;
+        private System.Windows.Forms.Button fontBtn;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace list_box_add_tems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text != "")
+            {
+                listBox1.Items.Add(this.textBox1.Text);
+            }
+
+            else
+            {
+                MessageBox.Show("Please enter some text");
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Show();
+        }
+
+        private void clearSelectBtn_Click(object sender, EventArgs e)
+        {
+            if (this.listBox1.SelectedIndex >= 0)
+
+            {
+                this.listBox1.Items.RemoveAt(this.listBox1.SelectedIndex);
+            }
+        }
+
+        private void fontBtn_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Font = fontBtn.Font;
+            }
+        }
+    }
+}
